abbaf676e905e038846609ad3386b5e7415d61a7
tomschlick <tomschlick@users.noreply.github.com>
2024-09-03 13:45:00 +0000

    Fix styling

--- tests/MakeTest.php
+++ tests/MakeTest.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 use ReturnEarly\ActionsPattern\Commands\MakeActionCommand;
 
-it('can make a new action', function () {
+it('can make a new action', function (): void {
     $this->artisan(MakeActionCommand::class, ['name' => 'TestAction'])
         ->assertExitCode(0);
 
     $this->assertFileExists(app_path('Actions/TestAction.php'));
 });
 
-it('can make a new action with a custom namespace', function () {
+it('can make a new action with a custom namespace', function (): void {
     $this->artisan(MakeActionCommand::class, ['name' => 'Custom\\TestAction'])
         ->assertExitCode(0);
 
